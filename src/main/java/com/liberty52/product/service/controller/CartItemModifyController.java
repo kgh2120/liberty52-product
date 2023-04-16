871f66f47835f26430286e26e56552eca56947f4
parkcoldroad <coldstreet@naver.com>
2023-04-16 17:08:09 +0900

    feat: 장바구니 수정 컨트롤러 추가

---
+++ src/main/java/com/liberty52/product/service/controller/CartItemModifyController.java
@@ -0,0 +1,28 @@
+package com.liberty52.product.service.controller;
+
+import com.liberty52.product.service.applicationservice.CartItemModifyService;
+import com.liberty52.product.service.controller.dto.CartModifyRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class CartItemModifyController {
+
+  private final CartItemModifyService cartItemModifyService;
+
+  @PatchMapping("/carts/custom-products/{customProductId}")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void modifyCartItem(@RequestHeader(HttpHeaders.AUTHORIZATION) String authId, @RequestPart CartModifyRequestDto dto,
+      @RequestPart(value = "file",required = false) MultipartFile imageFile, @PathVariable String customProductId) {
+    cartItemModifyService.modifyCartItem(authId,dto,imageFile,customProductId);
+  }
+}
