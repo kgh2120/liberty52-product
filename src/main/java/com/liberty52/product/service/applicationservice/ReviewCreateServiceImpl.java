a88124124dc87f71e1610a2ece66fc2fb1eca090
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-04-22 16:04:10 +0900

    fix: 리뷰 생성 시 이미지 관련 NPE 예외 처리 추가 (#46)

--- src/main/java/com/liberty52/product/service/applicationservice/ReviewCreateServiceImpl.java
+++ src/main/java/com/liberty52/product/service/applicationservice/ReviewCreateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.liberty52.product.service.applicationservice;
 
 import com.liberty52.product.global.adapter.S3UploaderImpl;
+import com.liberty52.product.global.exception.external.BadRequestException;
 import com.liberty52.product.global.exception.external.CustomProductNotFoundExcpetion;
 import com.liberty52.product.global.exception.external.NotYourResourceException;
 import com.liberty52.product.global.exception.external.OrderNotFoundException;
@@ -35,38 +36,48 @@ public class ReviewCreateServiceImpl implements ReviewCreateService {
   private final S3UploaderImpl s3Uploader;
 
   @Override
-  public void createReview(String reviewerId, ReviewCreateRequestDto dto, List<MultipartFile> imageFiles, String orderId) {
+  public void createReview(String reviewerId, ReviewCreateRequestDto dto,
+      List<MultipartFile> imageFiles, String orderId) {
     Product product = productRepository.findByName(dto.getProductName())
         .orElseThrow(() -> new ProductNotFoundException(dto.getProductName()));
 
     Orders order = ordersRepository.findById(orderId)
         .orElseThrow(OrderNotFoundException::new);
 
-    customProductRepository.findByOrderIdAndProductId(orderId,product.getId())//해당 order가  해당 product를 구매했는지 확인
+    customProductRepository.findByOrderIdAndProductId(orderId,
+            product.getId())//해당 order가  해당 product를 구매했는지 확인
         .orElseThrow(CustomProductNotFoundExcpetion::new);
 
-    if(!(order.getAuthId().equals(reviewerId))){
-      throw new NotYourResourceException(reviewerId,order.getAuthId());
+    if (!(order.getAuthId().equals(reviewerId))) {
+      throw new NotYourResourceException(reviewerId, order.getAuthId());
     }
 
     Review review = Review.create(dto.getRating(), dto.getContent());
     review.associate(product);
     review.associate(order);
 
-    if(!(imageFiles.size() > Review.IMAGES_MAX_COUNT || ( imageFiles.get(0) != null && imageFiles.get(0).getSize()==0))){//이미지를 안 넣을거면 null 값 보내기
-      for (MultipartFile imageFile : imageFiles) {
-        String reviewImageUrl = s3Uploader.upload(imageFile);
-        review.addImage(ReviewImage.create(review, reviewImageUrl));
-      }
+    if (imageFiles != null){
+      addImage(imageFiles, review);
     }
     reviewRepository.save(review);
   }
+
 
   @Override
   public void createReply(String reviewerId, ReplyCreateRequestDto dto, String reviewId) {
     Review review = reviewRepository.findById(reviewId)
         .orElseThrow(() -> new ResourceNotFoundException("Review", "ID", reviewId));
-    Reply reply = Reply.create(dto.getContent(),reviewerId);
+    Reply reply = Reply.create(dto.getContent(), reviewerId);
     reply.associate(review);
+  }
+
+
+  private void addImage(List<MultipartFile> imageFiles, Review review) {
+    if (imageFiles.size() > Review.IMAGES_MAX_COUNT)
+      throw new BadRequestException(1 + " <= Size of images <= " + Review.IMAGES_MAX_COUNT);
+    for (MultipartFile imageFile : imageFiles) {
+      String reviewImageUrl = s3Uploader.upload(imageFile);
+      review.addImage(ReviewImage.create(review, reviewImageUrl));
+    }
   }
 }
