e07bcd1298c43ece2176d77450c0dd23d02448f8
JIN-YONG LEE <74762475+jinlee0@users.noreply.github.com>
2023-05-08 00:15:17 +0900

    chore: 리뷰답변수정 응답 코드 수정 (#115)

--- src/main/java/com/liberty52/product/service/controller/ReplyModifyController.java
+++ src/main/java/com/liberty52/product/service/controller/ReplyModifyController.java
@@ -18,7 +18,7 @@ public class ReplyModifyController {
     private final ReplyModifyService replyModifyService;
 
     @PutMapping("/reviews/{reviewId}/replies/{replyId}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void replyModify(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
                             @RequestHeader("LB-Role") String role,
                             @Validated @RequestBody ReplyModifyRequestDto dto,
