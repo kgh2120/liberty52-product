25725d25f5cd8ab4bbed2dffd66a9d823149609e
parkcoldroad <coldstreet@naver.com>
2023-05-18 16:26:21 +0900

    feat: 관리자 리뷰 상세 조회 response 추가

---
+++ src/main/java/com/liberty52/product/service/controller/dto/AdminReviewDetailResponse.java
@@ -0,0 +1,62 @@
+package com.liberty52.product.service.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.liberty52.product.service.entity.Review;
+import com.liberty52.product.service.entity.ReviewImage;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class AdminReviewDetailResponse {
+  private ReviewContent content;
+
+  @JsonIgnore
+  private Set<String> authorIds = new HashSet<>();
+  public AdminReviewDetailResponse(Review review) {
+    String orderAuthId = review.getCustomProduct().getOrders().getAuthId();
+    content = new ReviewContent(review.getRating(),review.getContent(),review.getReviewImages().stream().map(
+        ReviewImage::getUrl).toList(),review.getCreatedAt().toLocalDate(),orderAuthId,review.getReplies().stream().map(
+            rp -> new ReplyContent(rp.getAuthId(),rp.getContent(),rp.getId(),rp.getCreatedAt().toLocalDate())).toList());
+  }
+
+  @Data
+  public class ReviewContent{
+    private Integer rating;
+    private String content;
+    private List<String> imageUrls;
+    @JsonIgnore
+    private String authorId;
+    private LocalDate reviewCreatedAt;
+    private List<ReplyContent> replies;
+
+    public ReviewContent(Integer rating, String content, List<String> imageUrls, LocalDate reviewCreatedAt,
+        String authorId, List<ReplyContent> replies) {
+      this.rating = rating;
+      this.content = content;
+      this.imageUrls = imageUrls;
+      this.authorId = authorId;
+      this.reviewCreatedAt = reviewCreatedAt;
+      this.replies = replies;
+    }
+  }
+
+  @Data
+  public class ReplyContent{
+    @JsonIgnore
+    private String authorId;
+    private String content;
+    private String replyId;
+    private LocalDate replyCreatedAt;
+
+    public ReplyContent(String authorId, String content,String replyId, LocalDate replyCreatedAt) {
+      this.authorId = authorId;
+      this.content = content;
+      this.replyId = replyId;
+      this.replyCreatedAt = replyCreatedAt;
+    }
+  }
+}
